97b768e13384479157d99d2d08243efc78aba1b6
Rokurou-lmb <rokurou.lmb.github@gmail.com>
2015-04-06 23:37:03 +0200

    Finished the GraphParser class Added Documentation

--- src/lennart/magnus/borchert/GraphFramework/FileIO/GraphParser.java
+++ src/lennart/magnus/borchert/GraphFramework/FileIO/GraphParser.java
@@ -4,13 +4,8 @@ import java.util.List;
 
 import lennart.magnus.borchert.GraphFramework.Materialien.Vertex;
 
-import org.jgraph.graph.DefaultEdge;
-import org.jgrapht.Graph;
 import org.jgrapht.graph.DefaultWeightedEdge;
-import org.jgrapht.graph.DirectedMultigraph;
 import org.jgrapht.graph.DirectedWeightedMultigraph;
-import org.jgrapht.graph.Multigraph;
-import org.jgrapht.graph.WeightedMultigraph;
 
 /**
  * 
@@ -19,16 +14,16 @@ import org.jgrapht.graph.WeightedMultigraph;
  */
 public class GraphParser {
 
-	private static String graphOptionsLine = "(#directed)?(#attributed)?(#weigthed)?";
 	private static String directed = "#directed";
 	private static String attributed = "#attributed";
 	private static String weighted = "#weighted";
+	private boolean directedGraph = false;
 	private boolean attributedGraph = false;
+	private boolean weightedGraph = false;
 
-	public Graph parse(String file) {
-		// TODO finish parser
-		Graph<Vertex, DefaultEdge> graph;
-		
+	public DirectedWeightedMultigraph<Vertex, DefaultWeightedEdge> parse(String file) {
+		// TODO test parser
+
 		GraphReader fileReader = new GraphReader();
 		List<String> lines = null;
 		try {
@@ -39,30 +34,42 @@ public class GraphParser {
 		int i = 0;
 		String optionsLine = lines.get(i);
 
+		DirectedWeightedMultigraph<Vertex, DefaultWeightedEdge> graph = new DirectedWeightedMultigraph<>(DefaultWeightedEdge.class);
+		directedGraph = optionsLine.contains(directed);
 		attributedGraph = optionsLine.contains(attributed);
-		if (optionsLine.contains(directed)) {
-			if (optionsLine.contains(weighted)) {
-				Graph<Vertex, DefaultWeightedEdge> graph = new DirectedWeightedMultigraph<Vertex, DefaultWeightedEdge>(DefaultWeightedEdge.class);
-			}else {
-				Graph<Vertex, DefaultEdge> graph = new DirectedMultigraph<Vertex, DefaultEdge>(DefaultEdge.class);
-			}
-			i++;
-		}else if(optionsLine.contains(weighted)){
-			Graph<Vertex, DefaultWeightedEdge> graph = new WeightedMultigraph<Vertex, DefaultWeightedEdge>(DefaultWeightedEdge.class);
-			i++;
-		}else{
-			Graph<Vertex, DefaultEdge> graph = new Multigraph<Vertex, DefaultEdge>(DefaultEdge.class);
-		}
+		weightedGraph = optionsLine.contains(weighted);
+		DefaultWeightedEdge edge;
+		DefaultWeightedEdge invertedEdge;
+
 		for (; i < lines.size(); i++) {
 			String line = lines.get(i);
 			String[] vertices = line.split(",");
+			String[] sourceValues = vertices[0].split(":");
+			String[] targetValues = vertices[1].split(":");
+			Vertex source;
+			Vertex target;
 
-			String[] values = vertices[0].split(":");
-			Vertex source = new Vertex(values[0], Integer.parseInt(values[1]));
-			values = vertices[1].split(":");
-			Vertex target = new Vertex(values[0], Integer.parseInt(values[1]));
+			if (attributedGraph) {
+				source = new Vertex(sourceValues[0], Integer.parseInt(sourceValues[1]));
+				target = new Vertex(targetValues[0], Integer.parseInt(targetValues[1]));
+			} else {
+				source = new Vertex(sourceValues[0], 0);
+				target = new Vertex(targetValues[0], 0);
+			}
 
 			graph.addVertex(source);
+			graph.addVertex(target);
+			edge = new DefaultWeightedEdge();
+			invertedEdge = new DefaultWeightedEdge();
+			if (weightedGraph) {
+				graph.setEdgeWeight(edge, Integer.parseInt(targetValues[targetValues.length-1]));
+				graph.setEdgeWeight(invertedEdge, Integer.parseInt(targetValues[targetValues.length-1]));
+			}
+
+			graph.addEdge(source, target, edge);
+			if (!directedGraph) {
+				graph.addEdge(target, source, invertedEdge);
+			}
 		}
 		return graph;
 	}

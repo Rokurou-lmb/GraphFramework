27fe39aedcd33660226a1ff30010b917ea52afe3
Gery <gerhard.wagner@haw-hamburg.de>
2015-04-07 23:20:36 +0200

    init gui

---
+++ src/lennart/magnus/borchert/GraphFramework/GUI/main/MainFrame.java
@@ -0,0 +1,6 @@
+package lennart.magnus.borchert.GraphFramework.GUI.main;
+
+public class MainFrame {
+    //conntect part of the frame
+
+}
---
+++ src/lennart/magnus/borchert/GraphFramework/GUI/main/MainFrameUI.java
@@ -0,0 +1,15 @@
+package lennart.magnus.borchert.GraphFramework.GUI.main;
+
+public class MainFrameUI {
+
+    //setup JFrame
+
+    //set size etc...
+
+    //set menu
+        //clear graph
+        //import graph
+
+    //make visible
+
+}

bdeef5f4f1cd8146f6e7f5b214a738d6495cc066
Gery <gerhard.wagner@haw-hamburg.de>
2015-04-08 21:07:51 +0200

    gui graph layout

--- src/lennart/magnus/borchert/GraphFramework/GUI/main/GraphDisplayerUI.java
+++ src/lennart/magnus/borchert/GraphFramework/GUI/main/GraphDisplayerUI.java
@@ -1,10 +1,16 @@
 package lennart.magnus.borchert.GraphFramework.GUI.main;
 
+import com.jgraph.layout.JGraphFacade;
+import com.jgraph.layout.JGraphLayout;
+import com.jgraph.layout.graph.JGraphSimpleLayout;
+import com.jgraph.layout.organic.JGraphFastOrganicLayout;
+import com.jgraph.layout.organic.JGraphOrganicLayout;
 import org.jgraph.JGraph;
 import org.jgrapht.ext.JGraphModelAdapter;
 
 import javax.swing.*;
 import java.awt.*;
+import java.util.Map;
 
 /**
  * Created by Gery on 08.04.2015.
@@ -15,17 +21,30 @@ public class GraphDisplayerUI {
     private JGraph _graph;
 
     public GraphDisplayerUI(){
-        _graph = new JGraph();
+//        _graph = new JGraph();
         _main = new JPanel();
         _main.setPreferredSize(new Dimension(1024, 786));
-        _graph.setPreferredSize(new Dimension(1024, 786));
-        _main.add(_graph);
+//        _graph.setPreferredSize(new Dimension(1024, 786));
+//        _main.add(_graph);
     }
 
     public void setGraph(JGraphModelAdapter adapter){
-        _main.remove(_graph);
+        if(_graph!=null)_main.remove(_graph);
         _graph = new JGraph(adapter);
         _graph.setPreferredSize(new Dimension(1024, 786));
+
+        JGraphFacade jgf = new JGraphFacade(_graph);
+        JGraphLayout layout = new JGraphSimpleLayout(JGraphSimpleLayout.TYPE_CIRCLE);
+        layout.run(jgf);
+//        JGraphOrganicLayout layoutifier = new JGraphOrganicLayout();
+//        layoutifier.setRadiusScaleFactor(1);
+//        layoutifier.run(jgf);
+
+        final Map nestedMap = jgf.createNestedMap(true, true);
+        _graph.getGraphLayoutCache().edit(nestedMap);
+
+        _graph.getGraphLayoutCache().update();
+        _graph.refresh();
         _main.add(_graph);
         _main.updateUI();
     }

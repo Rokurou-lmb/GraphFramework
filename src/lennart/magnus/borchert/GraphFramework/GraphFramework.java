1bca677ad30a5d428fbf2d2bba053007b221349f
Rokurou-lmb <rokurou.lmb.github@gmail.com>
2015-04-09 00:01:37 +0200

    Added Method to find shortest way, using Dijkstar algorithm contained within the JGraphT Library

--- src/lennart/magnus/borchert/GraphFramework/GraphFramework.java
+++ src/lennart/magnus/borchert/GraphFramework/GraphFramework.java
@@ -1,10 +1,24 @@
 package lennart.magnus.borchert.GraphFramework;
 
+import java.util.List;
+
+import org.jgrapht.Graph;
+import org.jgrapht.alg.DijkstraShortestPath;
+import org.jgrapht.graph.DefaultWeightedEdge;
+
 import lennart.magnus.borchert.GraphFramework.GUI.main.MainFrame;
+import lennart.magnus.borchert.GraphFramework.Materialien.Vertex;
 
 public class GraphFramework {
+	
+	private DijkstraShortestPath<Vertex, DefaultWeightedEdge> _dijkstra;
 
 	public static void main(String[] args){
 		new MainFrame();
+	}
+	
+	public List<DefaultWeightedEdge> findShortestPath(Graph<Vertex, DefaultWeightedEdge> graph, Vertex source, Vertex target){
+		_dijkstra = new DijkstraShortestPath<Vertex, DefaultWeightedEdge>(graph, source, target);
+		return _dijkstra.getPath().getEdgeList();
 	}
 }

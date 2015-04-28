38f25d265e058ab28e567b19883a30113310cf9b
Rokurou-lmb <rokurou.lmb.github@gmail.com>
2015-04-29 00:06:13 +0200

    Created JUnit test class which is supposed to test all 3 ShortestPath Algorithms simultaniously

---
+++ src/lennart/magnus/borchert/GraphFramework/Algorithms/Tests/ShortestPathAlgorithmImplementation_T.java
@@ -0,0 +1,14 @@
+package lennart.magnus.borchert.GraphFramework.Algorithms.Tests;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class ShortestPathAlgorithmImplementation_T {
+	//TODO: Implement tests for all 3 ShortestPath Algorithms.
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}

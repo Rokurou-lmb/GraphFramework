2a448a736ecbd1d0bbe352a95a67475eef2dc6f7
Rokurou-lmb <rokurou.lmb.github@gmail.com>
2015-04-29 19:08:16 +0200

    Added new testgraph Added 2 new tests to FlexibleGraph_T to test 2 recently implemented methods

--- src/lennart/magnus/borchert/GraphFramework/Materials/Tests/FlexibleGraph_T.java
+++ src/lennart/magnus/borchert/GraphFramework/Materials/Tests/FlexibleGraph_T.java
@@ -1,36 +1,53 @@
 package lennart.magnus.borchert.GraphFramework.Materials.Tests;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.Set;
+
+import lennart.magnus.borchert.GraphFramework.FileIO.FileFormatException;
+import lennart.magnus.borchert.GraphFramework.FileIO.GraphParser;
+import lennart.magnus.borchert.GraphFramework.Materials.Edge;
 import lennart.magnus.borchert.GraphFramework.Materials.FlexibleGraph;
 import lennart.magnus.borchert.GraphFramework.Materials.Vertex;
 
-import org.jgrapht.graph.DefaultEdge;
-import org.jgrapht.graph.DefaultWeightedEdge;
 import org.junit.Before;
 import org.junit.Test;
 
-public class FlexibleGraph_T {
+public class FlexibleGraph_T {	// TODO Expand tests
 
-	private FlexibleGraph<Vertex, DefaultEdge> _graph1;
-	private FlexibleGraph<Vertex, DefaultEdge> _graph2;
+	private GraphParser _parser;
+	private String _path;
+
+	private FlexibleGraph<Vertex, Edge> _graph1;
+	private FlexibleGraph<Vertex, Edge> _graph2;
+	private FlexibleGraph<Vertex, Edge> _graph3;
+
 	private Vertex _v1;
 	private Vertex _v2;
 
 	@Before
-	public void init() {
-		_graph1 = new FlexibleGraph<>(true, true, DefaultWeightedEdge.class);
-		_graph2 = new FlexibleGraph<>(false, false, DefaultWeightedEdge.class);
+	public void init() throws IOException, FileFormatException {
+		_graph1 = new FlexibleGraph<>(true, true, Edge.class);
+		_graph2 = new FlexibleGraph<>(false, false, Edge.class);
 		_v1 = new Vertex("1", 0);
 		_v2 = new Vertex("2", 0);
 
 		_graph1.addVertex(_v1);
 		_graph1.addVertex(_v2);
-		DefaultWeightedEdge edge1 = (DefaultWeightedEdge)_graph1.addEdge(_v1, _v2);
+		Edge edge1 =_graph1.addEdge(_v1, _v2);
 		_graph1.setEdgeWeight(edge1, 10);
 
 		_graph2.addVertex(_v1);
 		_graph2.addVertex(_v2);
 		_graph2.addEdge(_v2, _v1);
+
+		_path = new File(".").getCanonicalPath()+"\\graphs\\junittest2.graph";
+		_parser = new GraphParser();
+		_graph3 = _parser.parse(_path);
 	}
 
 	@Test
@@ -47,15 +64,24 @@ public class FlexibleGraph_T {
 		assertEquals(10, _graph1.getEdgeWeight(_graph1.getEdge(_v1, _v2)),0.1);
 		assertEquals(1.0, _graph2.getEdgeWeight(_graph2.getEdge(_v2, _v1)), 0.1);
 	}
-	//TODO: add tests for the getOutgoingEdges function
 
 	@Test
 	public void testGetOutgoingEdges(){
-		
+		Vertex testVertex = new Vertex("b", 0);
+		Set<Edge> outgoingEdgeSet = _graph3.getOutgoingEdges(testVertex);
+		assertTrue(outgoingEdgeSet.size() == 2);
+		for (Edge edge : outgoingEdgeSet) {
+			assertTrue(_graph3.getEdgeSource(edge).equals(testVertex));
+		}
 	}
 
 	@Test
 	public void testGetIncomingEdges(){
-		
+		Vertex testVertex = new Vertex("d", 0);
+		Set<Edge> incomingEdgeSet = _graph3.getIncomingEdges(testVertex);
+		assertTrue(incomingEdgeSet.size() == 2);
+		for (Edge edge : incomingEdgeSet) {
+			assertTrue(_graph3.getEdgeTarget(edge).equals(testVertex));
+		}
 	}
 }

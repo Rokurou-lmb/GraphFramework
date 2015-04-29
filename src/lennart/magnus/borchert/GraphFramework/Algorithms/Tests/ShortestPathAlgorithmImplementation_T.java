5693bc529be545deb496fc5884f59afa3fbfc0b1
Rokurou-lmb <rokurou.lmb.github@gmail.com>
2015-04-29 23:40:01 +0200

    Inverted the order of DijstraShortestPath output List. Implemented Test to check ShortestPathAlgorithmImplementations against a failsafe Algorithm provided by the JGraph Library

--- src/lennart/magnus/borchert/GraphFramework/Algorithms/Tests/ShortestPathAlgorithmImplementation_T.java
+++ src/lennart/magnus/borchert/GraphFramework/Algorithms/Tests/ShortestPathAlgorithmImplementation_T.java
@@ -1,14 +1,82 @@
 package lennart.magnus.borchert.GraphFramework.Algorithms.Tests;
 
-import static org.junit.Assert.*;
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Set;
 
+import lennart.magnus.borchert.GraphFramework.Algorithms.AStarShortestPath;
+import lennart.magnus.borchert.GraphFramework.Algorithms.BreadthFirstSearchShortestPath;
+import lennart.magnus.borchert.GraphFramework.Algorithms.DijkstraShortestPath;
+import lennart.magnus.borchert.GraphFramework.Algorithms.ShortestPathAlgorithm;
+import lennart.magnus.borchert.GraphFramework.FileIO.FileFormatException;
+import lennart.magnus.borchert.GraphFramework.FileIO.GraphParser;
+import lennart.magnus.borchert.GraphFramework.Materials.Edge;
+import lennart.magnus.borchert.GraphFramework.Materials.FlexibleGraph;
+import lennart.magnus.borchert.GraphFramework.Materials.Vertex;
+
+import org.jgrapht.GraphPath;
+import org.junit.Before;
 import org.junit.Test;
 
 public class ShortestPathAlgorithmImplementation_T {
-	//TODO: Implement tests for all 3 ShortestPath Algorithms.
+
+	private GraphParser _parser;
+	private String _path;
+
+	private FlexibleGraph<Vertex, Edge> _graph1;
+	private FlexibleGraph<Vertex, Edge> _graph2;
+	private FlexibleGraph<Vertex, Edge> _graph3;
+
+	@Before
+	public void init() throws IOException{
+		_graph1 = new FlexibleGraph<>(true, true, Edge.class);
+		_graph2 = new FlexibleGraph<>(false, false, Edge.class);
+		
+		_path = new File(".").getCanonicalPath()+"\\graphs\\";
+
+		_parser = new GraphParser();
+		
+	}
+
+
+
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void test() throws FileFormatException {
+		String file = "bsp1.graph";
+		_graph3 = _parser.parse(_path+file);
+		Vertex startVertex = new Vertex("a", 1);
+		ShortestPathAlgorithm<Vertex, Edge> breadthFirstSearch = new BreadthFirstSearchShortestPath<>();
+		ShortestPathAlgorithm<Vertex, Edge> dijkstraShortestPath = new DijkstraShortestPath<>();
+		ShortestPathAlgorithm<Vertex, Edge> aStarShortestPath = new AStarShortestPath<>();
+
+		List<Vertex> breadthFirstSearchList;
+		List<Vertex> dijkstraShortestPathList;
+		List<Vertex> aStarShortestPathList;
+		System.out.println("File to be parsed and tested with: "+file);
+		Set<Vertex> vertexSet = _graph3.vertexSet();
+		for (Vertex endVertex : vertexSet) {
+			org.jgrapht.alg.DijkstraShortestPath<Vertex, Edge> dijkstraSafe = new org.jgrapht.alg.DijkstraShortestPath<>(_graph3, startVertex, endVertex);
+			breadthFirstSearchList = breadthFirstSearch.findShortestPath(_graph3, startVertex, endVertex);
+			dijkstraShortestPathList = dijkstraShortestPath.findShortestPath(_graph3, startVertex, endVertex);
+			aStarShortestPathList = aStarShortestPath.findShortestPath(_graph3, startVertex, endVertex);
+
+			GraphPath<Vertex, Edge> dijkstraPathSafe = dijkstraSafe.getPath();
+			List<Vertex> dijkstraSafeList = new ArrayList<>();
+			dijkstraSafeList.add(dijkstraPathSafe.getStartVertex());
+			List<Edge> dijkstraSafeEdgeList = dijkstraSafe.getPathEdgeList();
+			if(dijkstraSafeEdgeList != null){
+				for (Edge edge : dijkstraSafeEdgeList) {
+					dijkstraSafeList.add(_graph3.getEdgeTarget(edge));
+				}
+			}
+
+			System.out.println("Shortest path from "+startVertex.getIdentifier()+" to "+endVertex.getIdentifier()+" with BFS:               "+breadthFirstSearchList.toString());
+			System.out.println("Shortest path from "+startVertex.getIdentifier()+" to "+endVertex.getIdentifier()+" with Dijkstra:          "+dijkstraShortestPathList.toString());
+			System.out.println("Shortest path from "+startVertex.getIdentifier()+" to "+endVertex.getIdentifier()+" with AStar:             "+aStarShortestPathList.toString());
+			System.out.println("Shortest path from "+startVertex.getIdentifier()+" to "+endVertex.getIdentifier()+" with Dijkstra(failsafe):"+dijkstraSafeList.toString());
+		}
 	}
 
 }

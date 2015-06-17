68e1fbfa9b76a67f3b98487453f675ef95cad360
Gery <gerhard.wagner@haw-hamburg.de>
2015-06-17 21:40:11 +0200

    fleury with error

--- src/lennart/magnus/borchert/GraphFramework/Algorithms/EulerTour/FleuryEulerTour.java
+++ src/lennart/magnus/borchert/GraphFramework/Algorithms/EulerTour/FleuryEulerTour.java
@@ -16,38 +16,37 @@ import java.util.List;
 public class FleuryEulerTour<V, E> implements EulerTourAlgorithm<V, E>{
 
 	@Override
-	public GraphPathImpl<V, E> findEulerCircle(Graph graph) {
+	public GraphPathImpl<V, E> findEulerCircle(Graph<V,E> graph) {
 
-		Graph<Vertex,Edge> _graph = graph;
-		Graph<Vertex,Edge> eulerGraph = new FlexibleGraph<>(((FlexibleGraph)graph).isDirected(),((FlexibleGraph)graph).isWeighted(),Edge.class);
-		for(Edge e : _graph.edgeSet()){
-			Vertex s = _graph.getEdgeSource(e);
+		Graph<V,E> eulerGraph = new FlexibleGraph<V,E>(((FlexibleGraph)graph).isDirected(),((FlexibleGraph)graph).isWeighted(),Edge.class);
+		for(E e : graph.edgeSet()){
+			V s = graph.getEdgeSource(e);
 			eulerGraph.addVertex(s);
-			Vertex t = _graph.getEdgeTarget(e);
+			V t = graph.getEdgeTarget(e);
 			eulerGraph.addVertex(t);
 			eulerGraph.addEdge(s,t, e);
 		}
 
-		Vertex w0 = (Vertex) graph.vertexSet().iterator().next();
+		V w0 = graph.vertexSet().iterator().next();
 		List<E> marked = new LinkedList<>();
 
 		ShortestPathAlgorithm bfs = new BreadthFirstSearchShortestPath<>();
 
 		while (marked.size() < graph.edgeSet().size()){
 			//get an edge candidate
-			Edge candidate = getEdgeCandidate(eulerGraph,w0,bfs);
-			if(candidate != null && ((FlexibleGraph<Vertex,Edge>) _graph).getOutgoingEdges(w0).size()%2==0){
-				marked.add((E)candidate);
-				if (w0.getIdentifier().equals((_graph.getEdgeTarget(candidate)).getIdentifier())) {
-					w0 = _graph.getEdgeSource(candidate);
-				}else if (w0.getIdentifier().equals((_graph.getEdgeSource(candidate)).getIdentifier())){
-					w0 = _graph.getEdgeTarget(candidate);
+			E candidate = getEdgeCandidate(eulerGraph,w0,bfs);
+			if(candidate != null && ((FlexibleGraph<V,E>) graph).getOutgoingEdges(w0).size()%2==0){
+				marked.add(candidate);
+				if (((Vertex)w0).getIdentifier().equals(((Vertex)(graph.getEdgeTarget(candidate))).getIdentifier())) {
+					w0 = graph.getEdgeSource(candidate);
+				}else if (((Vertex)w0).getIdentifier().equals((((Vertex) graph.getEdgeSource(candidate))).getIdentifier())){
+					w0 = graph.getEdgeTarget(candidate);
 				}
 			}else {
-				return new GraphPathImpl<V,E>(graph,(V)w0,(V)w0,new ArrayList<>(),0);
+				return new GraphPathImpl<>(graph,w0,w0,new ArrayList<>(),0);
 			}
 		}
-		return new GraphPathImpl<V,E>(graph,(V)w0,(V)w0,marked,0);
+		return new GraphPathImpl<>(graph,w0,w0,marked,0);
 	}
 
 	/**
@@ -57,11 +56,11 @@ public class FleuryEulerTour<V, E> implements EulerTourAlgorithm<V, E>{
 	 * @param spa the algorithm we are using to test for bridge
 	 * @return <code>Edge</code> if there is one <code>null</code> if there is none
 	 */
-	private Edge getEdgeCandidate(Graph<Vertex,Edge> graph,Vertex w0, ShortestPathAlgorithm spa){
-		Iterator<Edge> edgeIterator = ((FlexibleGraph<Vertex,Edge>) graph).getOutgoingEdges(w0).iterator();
-		Vertex candidateS;
-		Vertex candidateT;
-		Edge candidate = null;
+	private E getEdgeCandidate(Graph<V,E> graph,V w0, ShortestPathAlgorithm spa){
+		Iterator<E> edgeIterator = ((FlexibleGraph<V,E>) graph).getOutgoingEdges(w0).iterator();
+		V candidateS;
+		V candidateT;
+		E candidate = null;
 		while (edgeIterator.hasNext()){
 			candidate = edgeIterator.next();
 			candidateS = graph.getEdgeSource(candidate);
